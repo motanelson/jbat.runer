bc36e20cd836e542c624ad2e1dbd2cd3e4fee26d
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-05 06:50:37 +0100

    Delete batruner.py

--- batruner.py
+++
@@ -1,9 +0,0 @@
-import os
-import copy
-compilers="openjdk-asmtools-jasm $1.jbat -w . "
-print("\033c\033[47;31m\ngive me file  jbat: to run ? \n")
-#a="Hello.java"
-a=input().strip()
-b=a.replace(".jbat","")
-os.system(compilers.replace("$1",b)  )
-os.system("java $1".replace("$1",b))
